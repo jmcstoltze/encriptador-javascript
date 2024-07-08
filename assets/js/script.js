9a2dc265366a74a0e56e1e3321f85fb581f60445
Jose Contreras Stoltze <jmcontreras.stoltze@gmail.com>
2024-07-07 23:43:16 -0400

    commit segundo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,8 @@
-/*
-Requerimientos de encriptación
-
-Las "llaves" de encriptación que utilizaremos son las siguientes:
-La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat" */
-
-/* 
-let textoErroneo = "3783UUUUú$#%";
-let texto = "hola como estas";
-let textoEncriptado = "hoberlai cobermober enterstais"; */
-
 // Valida que un texto ingresado como parámetro contenga solo minúsculas y no esté vacío
 function validarTexto(texto) {
     var regex = /^[a-z\s]*$/; // Expresión regular para validar solo letras minúsculas
     return regex.test(texto) && texto.trim !== '';
 }
-
-/*
-console.log(validarTexto(texto)); // true
-console.log(validarTexto(textoErroneo)); // false */
 
 // Recibe texto y lo encripta
 function encriptarTexto(texto) {
@@ -52,76 +33,50 @@ function desencriptarTexto(textoEncriptado) {
     return textoDesencriptado;
 }
 
-/*
-console.log(encriptarTexto(texto)); // hoberlai cobermober enterstais
-console.log(desencriptarTexto(textoEncriptado)); // hola como estas */
-
-
-// Captura el texto y lo retorna encriptado
-function capturarTextoEncriptar() {
+// Encripta el texto
+function Encriptar() {
     
     let textoCapturado = document.getElementById('texto').value;
-    let textoValido = validarTexto(textoCapturado);
-    let mensajeValidacion = document.getElementById('mensajeValidacion');
     let textoCapturadoEncriptado = '';
     
     // Si el texto es válido
     if (validarTexto(textoCapturado)) {
-        // console.log(textoCapturado);
         textoCapturadoEncriptado = encriptarTexto(textoCapturado);
-        // console.log(textoCapturadoEncriptado);
-    } else {
-        mensajeValidacion.style.color = 'red';
-        // console.log('No es texto válido');
+        imprimirEnPantalla(textoCapturadoEncriptado);
     }
-
-    // Mensaje de validación aparece y desaparece según corresponda
-    mensajeValidacion.style.display = textoValido ? 'none' : 'block';
-
-    modificaSidebar(textoCapturadoEncriptado);
 }
 
-// Captura el texto y lo retorna desencriptado
-function capturarTextoDesencriptar() {
+// Desencripta el texto
+function Desencriptar() {
 
     let textoCapturado = document.getElementById('texto').value;
-    let textoValido = validarTexto(textoCapturado);
-    let mensajeValidacion = document.getElementById('mensajeValidacion');
     let textoCapturadoDesencriptado = '';
     
     // Si el texto es válido
     if (validarTexto(textoCapturado)) {
-        // console.log(textoCapturado);
         textoCapturadoDesencriptado = desencriptarTexto(textoCapturado);
-        // console.log(textoCapturadoDesencriptado);
-    } else {
-        mensajeValidacion.style.color = 'red';
-        // console.log('No es texto válido');
+        imprimirEnPantalla(textoCapturadoDesencriptado);
     }
-
-    // Mensaje de validación aparece y desaparece según corresponda
-    mensajeValidacion.style.display = textoValido ? 'none' : 'block';
-    
-    modificaSidebar(textoCapturadoDesencriptado);
 }
 
-// Agregar evento input al textarea para validar en tiempo real
-document.getElementById('texto').addEventListener('input', capturarTextoEncriptar);
-
-// Muestra el resultado en pantalla
-function modificaSideBar(texto) {
-
-    let textoProcesado = texto;
+// Función que se ejecuta en tiempo real mientras se escribe en el textarea
+function validarEnTiempoReal() {
+    let textoCapturado = document.getElementById('texto').value;
+    let textoValido = validarTexto(textoCapturado);
+    let mensajeValidacion = document.getElementById('mensajeValidacion');
     let sidebarTitulo = document.getElementById('sidebarTitulo');
     let sidebarParrafo = document.getElementById('sidebarParrafo');
 
-    if (textoProcesado !== '') {
-        sidebarTitulo.style.display = 'none';
-        sidebarParrafo.style.display = 'none';
-    }
-
-    return textoProcesado; // Retorna el texto para imprimir en pantalla
+    // Desaparece o se modifica información en pantalla según corresponda
+    mensajeValidacion.style.display = textoValido ? 'none' : 'block';
+    mensajeValidacion.style.color = textoValido ? 'black' : 'red';
+    sidebarTitulo.style.display = 'none';
+    sidebarParrafo.style.display = 'none';
+    sidebarImagen.style.display = 'none';
 }
+
+// Agregar evento input al textarea para validar en tiempo real
+document.getElementById('texto').addEventListener('input', validarEnTiempoReal);
 
 // Imprime en pantalla el resultado
 function imprimirEnPantalla(texto) {
@@ -131,6 +86,3 @@ function imprimirEnPantalla(texto) {
         resultado.innerHTML = texto;
     }
 }
-
-
-
